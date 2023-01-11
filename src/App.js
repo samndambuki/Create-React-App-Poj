f3824f6794b0489866bc4a5fe0f7944200584b31
samndambuki <sam.ndambuki08@gmail.com>
2023-01-11 10:34:29 +0300

    UseReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
 //const cities = ["Tokyo","Tahoe City","Bend"];
 //console.log(cities[0]);
@@ -10,27 +10,37 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [emotion,setEmotion] = useState("happy");
-  const [secondary,setSecondary] = 
-  useState("tired");
+  // const [emotion,setEmotion] = useState("happy");
+  // const [secondary,setSecondary] = 
+  // useState("tired");
 
   //console.log(what);
 
-  useEffect(()=>{
-    console.log(`Its ${emotion} right now`);
-  }, [emotion]);
+  // useEffect(()=>{
+  //   console.log(`Its ${emotion} right now`);
+  // }, [emotion]);
 
-  useEffect(()=>{
-    console.log(`Its ${secondary} around here`);
-  },[secondary]);
+  // useEffect(()=>{
+  //   console.log(`Its ${secondary} around here`);
+  // },[secondary]);
+
+  const [checked,setChecked] = useReducer((checked) => !checked,false);
 
   return (
     <div className="App">
-     <h1>Current emotion is {emotion}</h1>
+
+     {/* <h1>Current emotion is {emotion}</h1>
      <button onClick={()=> setEmotion("sad")}>Sad</button>
      <button onClick={()=> setEmotion("excited")}>Excited</button>
      <h2>Current Secondary emotion is {secondary}</h2>
-     <button onClick={()=> setSecondary("grateful")}>Grateful</button>
+     <button onClick={()=> setSecondary("grateful")}>Grateful</button> */}
+
+     <input type="checkbox" value={checked}
+     onChange={setChecked}
+     />
+
+     <label>{checked? "checked" : "not checked"}</label>
+
     </div>
   );
 }

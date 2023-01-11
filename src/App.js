da94e6ab7cf101a9d4e0fd9ac0537c4fa93d5bab
samndambuki <sam.ndambuki08@gmail.com>
2023-01-11 10:10:25 +0300

    Dependency Array

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { useState, useEffect } from 'react';
 function App() {
 
   const [emotion,setEmotion] = useState("happy");
+  const [secondary,setSecondary] = 
+  useState("tired");
 
   //console.log(what);
 
@@ -18,11 +20,17 @@ function App() {
     console.log(`Its ${emotion} right now`);
   }, [emotion]);
 
+  useEffect(()=>{
+    console.log(`Its ${secondary} around here`);
+  },[secondary]);
+
   return (
     <div className="App">
      <h1>Current emotion is {emotion}</h1>
      <button onClick={()=> setEmotion("sad")}>Sad</button>
      <button onClick={()=> setEmotion("excited")}>Excited</button>
+     <h2>Current Secondary emotion is {secondary}</h2>
+     <button onClick={()=> setSecondary("grateful")}>Grateful</button>
     </div>
   );
 }

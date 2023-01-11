25152613ab24ce4af5fe664bf764190d3783b085
samndambuki <sam.ndambuki08@gmail.com>
2023-01-11 09:47:29 +0300

    Use State Hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
+import { useState } from 'react';
+
 
 //const cities = ["Tokyo","Tahoe City","Bend"];
 //console.log(cities[0]);
 
-const [firstcity,second] = ["Tokyo","Tahoe City","Bend"];
-console.log(firstcity);
-console.log(second);
+// const [firstcity,second] = ["Tokyo","Tahoe City","Bend"];
+// console.log(firstcity);
+// console.log(second);
 
-function App({library}) {
+function App() {
+
+  const [emotion,setEmotion] = useState("happy");
+
+  //console.log(what);
+
   return (
     <div className="App">
-     <h1>Hello From {library}</h1>
+     <h1>Current emotion is {emotion}</h1>
+     <button onClick={()=> setEmotion("sad")}>Sad</button>
+     <button onClick={()=> setEmotion("excited")}>Excited</button>
     </div>
   );
 }

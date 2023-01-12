05a2362581bf7376e3604ecb3b7af57692c70314
samndambuki <sam.ndambuki08@gmail.com>
2023-01-12 22:39:53 +0300

    loading states

--- src/App.js
+++ src/App.js
@@ -69,20 +69,30 @@ function App() {
   // const [checked,setChecked] = useReducer((checked) => !checked,false);
 
   const [data,setData] = useState(null);
+  const [error,setError] = useState(null);
+  const[loading,setLoading] = useState(false);
+
   useEffect(()=>{
+    setLoading(true);
     fetch(`https://api.github.com/users/samndambuki`)
   .then((response)=>response.json())
   .then(setData)
+  .then(()=>setLoading(false))
+  .catch(setError);
   },[]);
 
-  if(data) return (
+  if(loading) return <h1>Loading...</h1>;
+  if(error) return <pre>{JSON.stringify(error)}</pre>;
+  if(!data) return null;
+   
+  return (
     <GithubUser 
     name={data.name} 
     location={data.location}
     avatar={data.avatar_url}/>
   );
 
-  return (
+  // return (
 
     // // <form onSubmit={submit}>
     // //   <input 
@@ -109,9 +119,9 @@ function App() {
 
     // </form>
 
-    <h1>Data</h1>
+    // <h1>Data</h1>
 
-  );
+  // );
 }
 
 export default App;
